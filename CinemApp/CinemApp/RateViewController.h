89ed6ed7f36d92f460ae6697571aa4bf0a33315e
Mikael Rosquist <mikael@rosquist.se>
2014-02-11 22:49:40 +0100

    äntligen fixat den där jävla slidern

---
+++ CinemApp/CinemApp/RateViewController.h
@@ -0,0 +1,13 @@
+//
+//  RateViewController.h
+//  CinemApp
+//
+//  Created by mikael on 11/02/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RateViewController : UIViewController
+
+@end
---
+++ CinemApp/CinemApp/RateViewController.m
@@ -0,0 +1,38 @@
+//
+//  RateViewController.m
+//  CinemApp
+//
+//  Created by mikael on 11/02/14.
+//
+//
+
+#import "RateViewController.h"
+
+@interface RateViewController ()
+
+@end
+
+@implementation RateViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
