9476c5ede022a85d37e0b328745e50713df6448d
teodorostlund <teo.ostlund@gmail.com>
2014-02-13 13:38:40 +0100

    Fixat menyn

---
+++ CinemApp/CinemApp/tabBarController.h
@@ -0,0 +1,13 @@
+//
+//  tabBarController.h
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-02-12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface tabBarController : UITabBarController
+
+@end
