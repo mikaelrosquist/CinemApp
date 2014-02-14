91f1a540028e6aa826230e6bfb0dab5446193427
teodorostlund <teo.ostlund@gmail.com>
2014-02-14 16:02:20 +0100

    NU SKA DET FUNKA

--- CinemApp/CinemApp/ProfileViewController.m
+++ CinemApp/CinemApp/ProfileViewController.m
@@ -7,18 +7,40 @@
 //
 
 #import "ProfileViewController.h"
-#import <Parse/Parse.h>
+#import "UIImage+ImageEffects.h"
 
 @interface ProfileViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *profileBackDropImage;
+@property (weak, nonatomic) IBOutlet UILabel *profileNameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePlaceHolderImage;
+
+@property (nonatomic) UIImage *image;
+@property (nonatomic) UIImage *profilePictureImage;
 
 @end
 
 @implementation ProfileViewController
-@synthesize profileName;
+
+@synthesize profilePictureImage;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+   
+    self.image = [UIImage imageNamed:@"kitten"];
+    self.profilePictureImage = [UIImage imageNamed:@"profilePlaceHolder"];
+
+    NSString *name = @"Firstname Lastname";
+    UIImage *effectImage = [self.image applyDarkEffect];
+
+    self.profileNameLabel.textColor = [UIColor whiteColor];
+
+    self.profileBackDropImage.image = effectImage;
+    self.profilePlaceHolderImage.image = profilePictureImage;
+    self.profileNameLabel.text = name;
+    
+/*
+ //TEST LOGIN
     [PFUser logInWithUsername:@"admin" password:@"admin"];
     
     if ([PFUser currentUser]) {
@@ -26,12 +48,8 @@
     } else {
         self.profileName.text = @"Not logged in";
     }
-
+*/
     
-}
-- (IBAction)logOut:(id)sender {
-    [PFUser logOut];
-    [profileName setNeedsDisplay];
 }
 
 - (void)didReceiveMemoryWarning
