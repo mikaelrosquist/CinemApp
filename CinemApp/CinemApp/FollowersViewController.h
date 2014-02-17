b71abc7e1854f924c8b9187ba1ddba6992bdddc9
teodorostlund <teo.ostlund@gmail.com>
2014-02-17 16:39:00 +0100

    Synk

---
+++ CinemApp/CinemApp/FollowersViewController.h
@@ -0,0 +1,14 @@
+//
+//  FollowersViewController.h
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-02-17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FollowersViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *topScrollView;
+
+@end
