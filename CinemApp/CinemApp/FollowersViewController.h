94ea48ed3702537fd7a5d0a14bbae979c4dbe3e1
teodorostlund <teo.ostlund@gmail.com>
2014-02-17 13:15:43 +0100

    Inte ändrat mkt

---
+++ CinemApp/CinemApp/FollowersViewController.h
@@ -0,0 +1,13 @@
+//
+//  FollowersViewController.h
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-02-17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FollowersViewController : UIViewController
+
+@end
---
+++ CinemApp/CinemApp/FollowersViewController.m
@@ -0,0 +1,38 @@
+//
+//  FollowersViewController.m
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-02-17.
+//
+//
+
+#import "FollowersViewController.h"
+
+@interface FollowersViewController ()
+
+@end
+
+@implementation FollowersViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
