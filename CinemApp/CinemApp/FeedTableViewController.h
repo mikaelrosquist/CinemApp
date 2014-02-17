c540d72fb9c90e8ff8c2e731189474dddc555ba3
Mikael Rosquist <mikael@rosquist.se>
2014-02-17 14:37:41 +0100

    feedController tillagd plus table i feed

---
+++ CinemApp/CinemApp/FeedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedTableViewController.h
+//  CinemApp
+//
+//  Created by mikael on 17/02/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedTableViewController : UITableViewController
+
+@end
