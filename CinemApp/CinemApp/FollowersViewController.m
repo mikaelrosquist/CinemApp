f35eaa9e2fa8e9b0b364e891668bdd363803db63
teodorostlund <teo.ostlund@gmail.com>
2014-02-17 13:15:51 +0100

    Hej

--- CinemApp/CinemApp/FollowersViewController.m
+++ CinemApp/CinemApp/FollowersViewController.m
@@ -14,19 +14,10 @@
 
 @implementation FollowersViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
