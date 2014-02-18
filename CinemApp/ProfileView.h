441bfb0e442c792bf63dd947db76e14e290b3233
teodorostlund <teo.ostlund@gmail.com>
2014-02-18 13:48:59 +0100

    Skapat ProfileView

---
+++ CinemApp/ProfileView.h
@@ -0,0 +1,13 @@
+//
+//  ProfileView.h
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-02-18.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileView : UIView
+
+@end
---
+++ CinemApp/ProfileView.m
@@ -0,0 +1,31 @@
+//
+//  ProfileView.m
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-02-18.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import "ProfileView.h"
+
+@implementation ProfileView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
