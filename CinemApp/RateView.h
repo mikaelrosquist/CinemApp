b7f0f3a0226e2cf2864f1815b44324c0229fc8d6
Mikael Rosquist <mikael@rosquist.se>
2014-02-18 12:23:28 +0100

    ny RateView tillagd

---
+++ CinemApp/RateView.h
@@ -0,0 +1,13 @@
+//
+//  RateView.h
+//  CinemApp
+//
+//  Created by mikael on 18/02/14.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RateView : UIView
+
+@end
---
+++ CinemApp/RateView.m
@@ -0,0 +1,31 @@
+//
+//  RateView.m
+//  CinemApp
+//
+//  Created by mikael on 18/02/14.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import "RateView.h"
+
+@implementation RateView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
