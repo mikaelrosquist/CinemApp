ab714760ab02a14f8be6b79d6ed4d87bb2c03a73
teodorostlund <teo.ostlund@gmail.com>
2014-02-19 22:31:52 +0100

    Flyttat runt koden lite i ProfileViewController

--- CinemApp/ProfileViewController.m
+++ CinemApp/ProfileViewController.m
@@ -25,7 +25,84 @@ static CGFloat ImageWidth  = 320.0;
     TestView *tV;
 }
 
-@synthesize settingsButton;
+@synthesize settingsButton, tV;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        tV = [[TestView alloc]initWithFrame:CGRectMake(0, ImageHeight+10, 320, 200)];
+        self.title = @"username";
+        image = [UIImage imageNamed:@"kitten"];
+        imageWithBlur = [UIImage imageNamed:@"kitten"];
+        profilePictureImage = [UIImage imageNamed:@"profilePicPlaceHolder"];
+        
+        self.profilePictureImageView = [[UIImageView alloc] initWithImage:profilePictureImage];
+        self.profilePictureImageView.frame = CGRectMake(120, 60, 80, 80);
+        
+        
+        label = [[UILabel alloc] initWithFrame:CGRectMake(0, 10, 320, 40)];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.textColor=[UIColor whiteColor];
+        label.text = @"Firstname Lastname";
+        [label setFont:[UIFont fontWithName: @"HelveticaNeue-Light" size: 20.0f]];
+        
+        self.imgProfile = [[UIImageView alloc] initWithImage:image];
+		self.imgProfile.frame = CGRectMake(0, 0, ImageWidth, ImageHeight);
+        self.imgProfile.contentMode = UIViewContentModeScaleAspectFill;
+        
+        self.imgWithBlur = [[UIImageView alloc] initWithImage:imageWithBlur];
+		self.imgWithBlur.frame = CGRectMake(0, 0, ImageWidth, ImageHeight);
+        self.imgWithBlur.contentMode = UIViewContentModeScaleAspectFill;
+        
+        self.imgWithBlur.image = [image applyDarkEffect];
+        
+        /*
+        settingsButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        settingsButton.frame = CGRectMake(283, 36, 25, 25);
+        
+        [settingsButton setBackgroundImage:[UIImage imageNamed:@"settings_icon"]forState:UIControlStateNormal];
+        */
+        
+        UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Recent ratings", @"Highest ratings", nil]];
+        segmentedControl.frame = CGRectMake(10, ImageHeight+10, 300, 29);
+        segmentedControl.selectedSegmentIndex = 0;
+        segmentedControl.tintColor = [UIColor colorWithRed:1.000 green:0.314 blue:0.329 alpha:1];
+        [segmentedControl addTarget:self action:@selector(valueChanged:) forControlEvents: UIControlEventValueChanged];
+
+        UIView *fakeView = [[UIView alloc] init];
+        
+        CGRect frame = fakeView.frame;
+        frame.size.width = 320;
+        frame.size.height = tV.frame.size.height;
+        frame.origin.y = ImageHeight;
+        fakeView.frame = frame;
+        
+        self.scrollView = [[UIScrollView alloc] init];
+		self.scrollView.delegate = self;
+        self.scrollView.backgroundColor = [UIColor clearColor];
+        self.scrollView.contentSize = CGSizeMake(320, frame.size.height+ImageHeight);
+        
+        [self.scrollView addSubview:fakeView];
+        [self.view addSubview:self.imgProfile];
+        [self.view addSubview:self.imgWithBlur];
+        [self.scrollView addSubview:self.profilePictureImageView];
+        [self.scrollView addSubview:label];
+        [self.scrollView addSubview:settingsButton];
+        [self.view addSubview:self.scrollView];
+        [self.scrollView addSubview:tV];
+        [self.scrollView addSubview:segmentedControl];
+        
+        [self setEdgesForExtendedLayout:UIRectEdgeNone];
+        [self setAutomaticallyAdjustsScrollViewInsets:NO];
+        
+    }
+    return self;
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
     CGFloat yOffset = self.scrollView.contentOffset.y;
@@ -59,83 +136,6 @@ static CGFloat ImageWidth  = 320.0;
     
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        self.title = @"username";
-        image = [UIImage imageNamed:@"kitten"];
-        imageWithBlur = [UIImage imageNamed:@"kitten"];
-        profilePictureImage = [UIImage imageNamed:@"profilePicPlaceHolder"];
-        
-        self.profilePictureImageView = [[UIImageView alloc] initWithImage:profilePictureImage];
-        self.profilePictureImageView.frame = CGRectMake(120, 60, 80, 80);
-        
-        
-        label = [[UILabel alloc] initWithFrame:CGRectMake(0, 10, 320, 40)];
-        label.textAlignment = NSTextAlignmentCenter;
-        label.textColor=[UIColor whiteColor];
-        label.text = @"Firstname Lastname";
-        [label setFont:[UIFont fontWithName: @"HelveticaNeue-Light" size: 20.0f]];
-        
-        self.imgProfile = [[UIImageView alloc] initWithImage:image];
-		self.imgProfile.frame = CGRectMake(0, 0, ImageWidth, ImageHeight);
-        self.imgProfile.contentMode = UIViewContentModeScaleAspectFill;
-        
-        self.imgWithBlur = [[UIImageView alloc] initWithImage:imageWithBlur];
-		self.imgWithBlur.frame = CGRectMake(0, 0, ImageWidth, ImageHeight);
-        self.imgWithBlur.contentMode = UIViewContentModeScaleAspectFill;
-        
-        
-        self.imgWithBlur.image = [image applyDarkEffect];
-        
-        settingsButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        settingsButton.frame = CGRectMake(283, 36, 25, 25);
-        
-        [settingsButton setBackgroundImage:[UIImage imageNamed:@"settings_icon"]forState:UIControlStateNormal];
-        
-        UIView *fakeView = [[UIView alloc] init];
-        
-        CGRect frame = fakeView.frame;
-        frame.size.width = 320;
-        frame.size.height = 400;
-        frame.origin.y = ImageHeight;
-        fakeView.frame = frame;
-        
-        self.scrollView = [[UIScrollView alloc] init];
-		self.scrollView.delegate = self;
-        self.scrollView.backgroundColor = [UIColor clearColor];
-        self.scrollView.contentSize = CGSizeMake(320, frame.size.height+ImageHeight);
-        
-        UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:[NSArray arrayWithObjects:@"Recent ratings", @"Highest ratings", nil]];
-        segmentedControl.frame = CGRectMake(10, ImageHeight+10, 300, 29);
-        segmentedControl.selectedSegmentIndex = 0;
-        segmentedControl.tintColor = [UIColor colorWithRed:1.000 green:0.314 blue:0.329 alpha:1];
-        [segmentedControl addTarget:self action:@selector(valueChanged:) forControlEvents: UIControlEventValueChanged];
-        
-        [self.scrollView addSubview:fakeView];
-        
-        [self.view addSubview:self.imgProfile];
-        [self.view addSubview:self.imgWithBlur];
-        [self.scrollView addSubview:self.profilePictureImageView];
-        [self.scrollView addSubview:label];
-        [self.scrollView addSubview:settingsButton];
-        [self.view addSubview:self.scrollView];
-        [self.scrollView addSubview:tV];
-        
-        [self.scrollView addSubview:segmentedControl];
-        
-        [self setEdgesForExtendedLayout:UIRectEdgeNone];
-        [self setAutomaticallyAdjustsScrollViewInsets:NO];
-        
-    }
-    return self;
-}
-
-- (UIStatusBarStyle)preferredStatusBarStyle {
-    return UIStatusBarStyleLightContent;
-}
-
 - (void)valueChanged:(UISegmentedControl *)segment {
     
     if(segment.selectedSegmentIndex == 0) {
@@ -157,7 +157,6 @@ static CGFloat ImageWidth  = 320.0;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    tV = [[TestView alloc]initWithFrame:CGRectMake(0, ImageHeight+10, 320, 200)];
 }
 
 - (void)didReceiveMemoryWarning
