addc2420c055dbbb110cada11710df8eb0be3d27
Mikael Rosquist <mikael@rosquist.se>
2014-02-20 13:17:33 +0100

    lite mer pill med filmsidan

---
+++ CinemApp/ActivityView.h
@@ -0,0 +1,13 @@
+//
+//  ActivityView.h
+//  CinemApp
+//
+//  Created by mikael on 20/02/14.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityView : UIView
+
+@end
---
+++ CinemApp/ActivityView.m
@@ -0,0 +1,31 @@
+//
+//  ActivityView.m
+//  CinemApp
+//
+//  Created by mikael on 20/02/14.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import "ActivityView.h"
+
+@implementation ActivityView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
