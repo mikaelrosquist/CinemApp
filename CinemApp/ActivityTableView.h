fa6fdadd274fe4e930873294d4c94214cba74d4f
Mikael Rosquist <mikael@rosquist.se>
2014-02-21 12:21:13 +0100

    fixat generisk ActivityTableView

---
+++ CinemApp/ActivityTableView.h
@@ -0,0 +1,13 @@
+//
+//  ActivityTableView.h
+//  CinemApp
+//
+//  Created by mikael on 21/02/14.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ActivityTableView : UITableView
+
+@end
---
+++ CinemApp/ActivityTableView.m
@@ -0,0 +1,31 @@
+//
+//  ActivityTableView.m
+//  CinemApp
+//
+//  Created by mikael on 21/02/14.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import "ActivityTableView.h"
+
+@implementation ActivityTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
