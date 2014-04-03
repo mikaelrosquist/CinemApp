9beda2c9fa02e73407fa0e9c2d9adb955df61a92
teodorostlund <teo.ostlund@gmail.com>
2014-04-03 15:06:57 +0200

    Småfix

---
+++ CinemApp/SearchResult.h
@@ -0,0 +1,13 @@
+//
+//  SearchResult.h
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-04-03.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResult : NSObject
+
+@end
---
+++ CinemApp/SearchResult.m
@@ -0,0 +1,13 @@
+//
+//  SearchResult.m
+//  CinemApp
+//
+//  Created by Teodor Östlund on 2014-04-03.
+//  Copyright (c) 2014 Rosquist Östlund. All rights reserved.
+//
+
+#import "SearchResult.h"
+
+@implementation SearchResult
+
+@end
